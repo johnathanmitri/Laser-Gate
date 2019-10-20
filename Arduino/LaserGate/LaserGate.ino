64166c5d56bfcd216c4c3dcdfd1dfcde179ef518
Johnathan Mitri <johnathanmitri@gmail.com>
2019-10-19 19:56:40 -0700

    Revert "Revert "Working""
    This reverts commit 2516004521252c67cb3a3b0467cb7eba3189cb33.

--- Arduino/LaserGate/LaserGate.ino
+++ Arduino/LaserGate/LaserGate.ino
@@ -1,10 +1,12 @@
 int Thresh = 130; //originally 130
-unsigned long intTime;
+unsigned long startTime;
 unsigned long endTime;
 unsigned long blockageTime;
+byte startByteArray[4];
+byte endByteArray[4];
 String msg;
 
-void setup() 
+void setup()
 {
   Serial.begin(57600);
   pinMode(A4, INPUT);
@@ -22,17 +24,16 @@ void loop()
   digitalWrite(13, LOW);
   Serial.print("supz");
   delay(20);
-  byte ByteArray[4];
-  
+
   while (Serial.available() == 0)
   {
     while (analogRead(A4) >= Thresh) //&& (Serial.available() == 0))
     { if (Serial.available() != 0)
         goto JustOutsideOfLoop;
     }
-    
-//=== At this point, laser is obscured.
-    intTime = micros();
+
+    //=== At this point, laser is obscured.
+    startTime = micros();
     digitalWrite(13, LOW);
 
     while (analogRead(A4) <= Thresh) //&& (Serial.available() == 0))
@@ -40,17 +41,21 @@ void loop()
         goto JustOutsideOfLoop;
     }
 
-//=== At this point, laser is visible again.    
+    //=== At this point, laser is visible again.
     digitalWrite(13, HIGH);
     endTime = micros();   //i would combine this, but its  buggy, so leave as is.
-    blockageTime = endTime - intTime;
 
-for (int i = 0; i < 4; i++)
-{ ByteArray[i] = blockageTime & 255;
-  blockageTime >>= 8;
-}
-    
-    Serial.write(ByteArray, 4);
+    for (int i = 0; i < 4; i++)
+    { 
+      startByteArray[i] = startTime & 255;
+      startTime >>= 8;
+
+      endByteArray[i] = endTime & 255;
+      endTime >>= 8;
+    }
+
+    Serial.write(endByteArray, 4);     //a slight obfuscation here by sending the end value first.
+    Serial.write(startByteArray, 4);   
   }
 
 JustOutsideOfLoop:
